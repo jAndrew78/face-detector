6fbdaef4b06379b5d089e5669119f401b58b02c7
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-06-13 16:11:58 -0500

    choose img, grayscale conversion, imshow

--- face_detector.py
+++ face_detector.py
@@ -1,7 +1,13 @@
 import cv2
 
 # LOAD PRE-TRAINED DATA FROM OPENCV (HAAR CASCADE ALGO)
-trained_face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# CHOOSE AN IMG, CONVERT TO GREYSCALE AND SHOW
+img = cv2.imread(r'C:/Users/andyt/Desktop/source/projects/face-detector/face1.png')
+grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Face Detector', grayscale_img)
+cv2.waitKey()
 
 
 print("Code Complete")
