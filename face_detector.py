2edfe05f12423eb24717a70095e3b238f4c52b29
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-06-13 22:41:16 -0500

    get face coordinates from grayscale img

--- face_detector.py
+++ face_detector.py
@@ -1,11 +1,19 @@
 import cv2
 
 # LOAD PRE-TRAINED DATA FROM OPENCV (HAAR CASCADE ALGO)
-trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+trained_face_data = cv2.CascadeClassifier(r'C:\Users\andyt\Desktop\source\projects\face-detector\haarcascade_frontalface_default.xml')
 
-# CHOOSE AN IMG, CONVERT TO GREYSCALE AND SHOW
+# CHOOSE AN IMG, CONVERT TO GREYSCALE
 img = cv2.imread(r'C:/Users/andyt/Desktop/source/projects/face-detector/face1.png')
 grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# FACE DETECTION
+face_coordinates = trained_face_data.detectMultiScale(grayscale_img)
+print(face_coordinates)
+
+
+
+
 cv2.imshow('Face Detector', grayscale_img)
 cv2.waitKey()
 
