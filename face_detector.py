92d7d2a2ec259cefa3adb4205ca7a2150efd5544
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-06-13 22:42:33 -0500

    draw rect around face using coords

--- face_detector.py
+++ face_detector.py
@@ -7,14 +7,13 @@ trained_face_data = cv2.CascadeClassifier(r'C:\Users\andyt\Desktop\source\projec
 img = cv2.imread(r'C:/Users/andyt/Desktop/source/projects/face-detector/face1.png')
 grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# FACE DETECTION
+# DETECT FACES, GENERATE COORDS AND DRAW RECT
 face_coordinates = trained_face_data.detectMultiScale(grayscale_img)
-print(face_coordinates)
+cv2.rectangle(img, (94, 38), (330+94, 330+38), (0, 255, 0), 2)
 
 
 
-
-cv2.imshow('Face Detector', grayscale_img)
+cv2.imshow('Face Detector', img)
 cv2.waitKey()
 
 
