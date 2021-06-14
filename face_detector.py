a967819bea328617354466ba18e8fab17064b363
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2021-06-13 22:45:22 -0500

    set up loop for video rather than img

--- face_detector.py
+++ face_detector.py
@@ -7,12 +7,15 @@ trained_face_data = cv2.CascadeClassifier(r'C:\Users\andyt\Desktop\source\projec
 img = cv2.imread(r'C:/Users/andyt/Desktop/source/projects/face-detector/face1.png')
 grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# DETECT FACES, GENERATE COORDS AND DRAW RECT
+# FACE DETECTION, GENERATE COORDS 
 face_coordinates = trained_face_data.detectMultiScale(grayscale_img)
-cv2.rectangle(img, (94, 38), (330+94, 330+38), (0, 255, 0), 2)
 
+# DRAW RECT ON ORIGINAL IMG
+# (x, y, w, h) = face_coordinates[0]  <-- for single face
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-
+# DISPLAY IMG 
 cv2.imshow('Face Detector', img)
 cv2.waitKey()
 
